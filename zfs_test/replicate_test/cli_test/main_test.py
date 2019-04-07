598c11c553cd4037001b7b59a4560576726e40f2
Alex Brandt <alunduil@gmail.com>
2019-04-06 21:37:25 -0700

    Add a test of verbose invocation of main.

--- zfs_test/replicate_test/cli_test/main_test.py
+++ zfs_test/replicate_test/cli_test/main_test.py
@@ -1,4 +1,4 @@
-from click.testing import CliRunner
+from click.testing import CliRunner, Result
 
 from zfs.replicate.cli.main import main
 
@@ -8,6 +8,20 @@ def test_invokes_without_stacktrace() -> None:
 
     runner = CliRunner()
     result = runner.invoke(main, ["-l", "alunduil", "-i", "mypy.ini", "example.com", "bogus", "bogus"])
-    assert isinstance(result.exception, SystemExit) or result.exception == FileNotFoundError(
-        2, "No such file or directory: '/usr/bin/env'"
+    assert isinstance(result.exception, SystemExit) or file_not_found_error(
+        result
     ), "Expected SystemExit or FileNotFoundError."
+
+
+def test_invokes_without_stacktrace_verbose() -> None:
+    """zfs-replicate --verbose -l alunduil -i mypy.ini example.com bogus bogus => No stacktrace"""
+
+    runner = CliRunner()
+    result = runner.invoke(main, ["--verbose", "-l", "alunduil", "-i", "mypy.ini", "example.com", "bogus", "bogus"])
+    assert isinstance(result.exception, SystemExit) or file_not_found_error(
+        result
+    ), "Expected SystemExit or FileNotFoundError."
+
+
+def file_not_found_error(result: Result) -> bool:
+    return bool(FileNotFoundError(2, "No such file or directory: '/usr/bin/env'") == result.exception)
