317a89e6eb5c43d6b66d643275e99e97b9d1437b
Alex Brandt <alunduil@gmail.com>
2019-04-20 20:13:20 -0700

    Release 1.1.4
    * Update pytest, isort, hypothesis, and mypy.
    * Add trivial generate test.
    * Add a test of verbose invocation of main.
    * Allow FileNotFoundError in main invoke test.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst") as fh:
 
 setup(
     name="zfs-replicate",
-    version="1.1.3",
+    version="1.1.4",
     description="ZFS Snapshot Replicator",
     long_description=README,
     url="https://github.com/alunduil/zfs-replicate",
