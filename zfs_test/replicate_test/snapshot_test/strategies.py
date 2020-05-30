4b401ef89f568401d8b1ba4122d927bb1b2756ca
Alex Brandt <alunduil@gmail.com>
2020-05-30 10:03:36 -0700

    Add generate tests found in the Haskell experiment.
    Going through the exercise of converting this project to Haskell showed
    some obvious missing test cases.  They're added here to ensure we remain
    true as we continue forward with this python version.

---
+++ zfs_test/replicate_test/snapshot_test/strategies.py
@@ -0,0 +1,21 @@
+"""Snapshot Hypothesis Strategies"""
+
+import string
+from typing import Any, Dict
+
+from hypothesis.strategies import SearchStrategy, fixed_dictionaries, integers, none, text
+
+from zfs.replicate.filesystem.type import filesystem
+from zfs.replicate.snapshot.type import Snapshot
+
+_NOT_WHITESPACE = [x for x in string.printable if x not in string.whitespace and x != "@"]
+
+_FILESYSTEMS = text(_NOT_WHITESPACE).map(lambda x: "a{x}").map(filesystem)
+
+_SNAPSHOTS_DICT: Dict[str, SearchStrategy[Any]] = {
+    "filesystem": _FILESYSTEMS,
+    "name": text(_NOT_WHITESPACE),
+    "timestamp": integers(),
+    "previous": none(),
+}
+SNAPSHOTS = fixed_dictionaries(_SNAPSHOTS_DICT).map(lambda kwargs: Snapshot(**kwargs))
---
+++ zfs_test/replicate_test/snapshot_test/type_test.py
@@ -0,0 +1,12 @@
+"""zfs.replicate.snapshot.type tests"""
+
+from zfs.replicate.filesystem.type import filesystem
+from zfs.replicate.snapshot.type import Snapshot
+
+
+def test_eq_ignore_previous() -> None:
+    """Snapshot.previous ignored in eq"""
+
+    zero = Snapshot(filesystem=filesystem(""), name="", previous=None, timestamp=0)
+    previous = Snapshot(filesystem=filesystem(""), name="", previous=zero, timestamp=0)
+    assert zero == previous
