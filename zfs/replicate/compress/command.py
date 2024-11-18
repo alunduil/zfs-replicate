b6cbdae6aa5a7ec56c74a540544f63ce834b40fa
Konstantin Bogdanov <thevar1able@users.noreply.github.com>
2024-11-18 10:50:50 +0100

    Replace `lz4c` with `lz4`

--- zfs/replicate/compress/command.py
+++ zfs/replicate/compress/command.py
@@ -8,7 +8,7 @@ from .type import Compression
 def command(compression: Compression) -> Tuple[str, str]:
     """Compress and decompress command strings for compression."""
     if compression == Compression.LZ4:
-        return ("/usr/bin/env - lz4c | ", "/usr/bin/env - lz4c -d | ")
+        return ("/usr/bin/env - lz4 | ", "/usr/bin/env - lz4 -d | ")
 
     if compression == Compression.OFF:
         return ("", "")
